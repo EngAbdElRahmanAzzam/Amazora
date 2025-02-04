f7a9df8a01fd45294fa301cef3e0fb0b1c65bf31
Eng_AbdElRahmanAzzam <122513601+EngAbdElRahmanAzzam@users.noreply.github.com>
2025-02-04 23:47:33 +0200

    root navbar

--- src/components/common/navbar.tsx
+++ src/components/common/navbar.tsx
@@ -1,4 +1,5 @@
 import CartIcon from "../../asserts/icons/cartIcon";
+import SearchIcon from "../../asserts/icons/searchIcon";
 
 export interface INavProps{
   title:string;
@@ -6,9 +7,9 @@ export interface INavProps{
 }
 const NavItem = ({title, text}:INavProps) => {
   return(
-    <div>
-      <p>{title}</p>
-      <p>{text}</p>
+    <div className="text-white flex flex-col justify-between">
+      <p className="text-sm font-thin">{title}</p>
+      <p className="text-lg font-light">{text}</p>
     </div>
   )
 }
@@ -16,21 +17,21 @@ const NavItem = ({title, text}:INavProps) => {
 const RootNavbar = () => {
 
   return (
-    <header className="px-4 py-3 bg-gray-600 flex justify-between items-center">
-        <img className="w-36" src="/amazora-logo.png" alt="amazora-logo" />
+    <header className="px-4 py-3 bg-gray-600 flex gap-3 justify-between items-center">
+        <img className="w-28" src="/amazora-logo.png" alt="amazora-logo" />
 
-        <div className="flex-grow">
-          <input className="w-full"/>
-          <label>
-              
+        <div className="flex-grow relative">
+          <input className="py-2 w-full"/>
+          <label className="bg-orange-300  p-2 absolute -right-0">
+              <SearchIcon />
           </label>
         </div>
-        <nav>
-            <NavItem/>
-            <NavItem/>
-            <NavItem/>
+        <nav className="flex gap-2">
+            <NavItem title="Hello Guest" text="Sign in"/>
+            <NavItem title="Returns" text="& Orders"/>
+            <NavItem title="Your" text="Prime"/>
         </nav>
-        <CartIcon />
+        <CartIcon className="size-7"/>
     </header>
   )
 }
